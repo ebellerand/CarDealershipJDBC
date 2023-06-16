ca0f654f94e7a571201916834be84068fdfb29ca
ebellerand <ebellerand@appdev.yearup.org>
2023-06-16 14:09:35 -0400

    Finished project

--- src/main/java/com/yearup/dealership/db/LeaseDao.java
+++ src/main/java/com/yearup/dealership/db/LeaseDao.java
@@ -3,6 +3,7 @@ package com.yearup.dealership.db;
 import com.yearup.dealership.models.LeaseContract;
 
 import javax.sql.DataSource;
+import java.rmi.dgc.Lease;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -16,5 +17,19 @@ public class LeaseDao {
 
     public void addLeaseContract(LeaseContract leaseContract) {
         // TODO: Implement the logic to add a lease contract
+        String query = "INSERT INTO sales_contracts (Contract_Id, vin, lease_start, lease_end, monthly_payment ) values (?, ?, ?, ?, ?) ";
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1,leaseContract.getContractId());
+            statement.setString(2, leaseContract.getVin());
+            statement.setDate(3, java.sql.Date.valueOf(leaseContract.getLeaseStart()));
+            statement.setDate(4, java.sql.Date.valueOf(leaseContract.getLeaseEnd()));
+            statement.setDouble(5, leaseContract.getMonthlyPayment());
+            int rows = statement.executeUpdate();
+            System.out.println(rows + " rows affected! ");
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- src/main/java/com/yearup/dealership/db/SalesDao.java
+++ src/main/java/com/yearup/dealership/db/SalesDao.java
@@ -16,7 +16,18 @@ public class SalesDao {
 
     public void addSalesContract(SalesContract salesContract) {
         // TODO: Implement the logic to add a sales contract
-        String query = "INSERT INTO sales_contracts (ContractId, vin, saleDate, price) values (?, ?, ?, ?) ";
+        String query = "INSERT INTO sales_contracts (Contract_Id, vin, sale_Date, price) values (?, ?, ?, ?) ";
+        try (Connection connection = dataSource.getConnection()) {
+            PreparedStatement statement = connection.prepareStatement(query);
+            statement.setInt(1,salesContract.getContractId());
+            statement.setString(2, salesContract.getVin());
+            statement.setDate(3, java.sql.Date.valueOf(salesContract.getSaleDate()));
+            statement.setDouble(4, salesContract.getPrice());
+            int rows = statement.executeUpdate();
+            System.out.println(rows + " rows affected! ");
 
+    } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
